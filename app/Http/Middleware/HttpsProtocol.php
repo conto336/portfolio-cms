94c80e7214fcf295f68ffc0f3e2e325523dc1f25
conto336 <carlos.conto.336@gmail.com>
2021-09-06 21:38:59 -0600

    activacion ssl

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class HttpsProtocol
 {
@@ -16,7 +17,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production')) {
+        if (!$request->secure() && App::environment('production')) {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
