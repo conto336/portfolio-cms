c723b4f43ea133d61d45f521564aab6872471760
conto336 <carlos.conto.336@gmail.com>
2021-09-07 03:30:30 +0000

    cambios en el server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
+
+// condition 2
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+// condition 3
+        if (env('APP_FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
